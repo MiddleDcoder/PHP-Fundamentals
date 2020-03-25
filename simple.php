7891fb2618c380f79083bd4a6e0ed2310c79d5fc
middledcoder <middledcoder55@gmail.com>
2020-03-25 13:17:39 +0800

    introduction

---
+++ simple.php
@@ -0,0 +1,16 @@
+<?php
+$message = "Hello World!";
+$title = "HTML Sample";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Fundamentals - <?=$title?></title>
+</head>
+<body>
+    <h1><?php echo $message ?></h1>
+</body>
+</html>
