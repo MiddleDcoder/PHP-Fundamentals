853559565ab98c23267ce06f71c87a98951d28ab
middledcoder <middledcoder55@gmail.com>
2020-03-31 15:18:54 +0800

    properly working through sessions

--- web/final.php
+++ web/final.php
@@ -28,27 +28,27 @@ $postedData = $_SESSION['formPostData'];
         <div id="Body">
             <div>
                 <label>Favorite Author:</label> 
-                <span><?=$_POST['author']?>&nbsp;</span>
+                <span><?=$postedData['author']?>&nbsp;</span>
             </div>		
             <div>
                 <label>Favorite Century:</label>
-                <span><?php echo implode(', ', $_POST['century']);?>&nbsp;</span>
+                <span><?php echo implode(', ', $postedData['century']);?>&nbsp;</span>
             </div>
             <div>
                 <label>Comments:</label>
-                <span><?=$_POST['comments']?>&nbsp;</span>
+                <span><?=$postedData['comments']?>&nbsp;</span>
             </div>
             <div>
                 <label>Name:</label>
-                <span><?=$_POST['name']?>&nbsp;</span>
+                <span><?=$postedData['name']?>&nbsp;</span>
             </div>
             <div>
                 <label>E-mail Address:</label>
-                <span><?=$_POST['email']?>&nbsp;</span>
+                <span><?=$postedData['email']?>&nbsp;</span>
             </div>
             <div>
                 <label>Receive Newsletter:</label>
-                <span><?=$_POST['newsletter']?>&nbsp;</span>
+                <span><?=$postedData['newsletter']?>&nbsp;</span>
             </div>
         </div>
 	</body>
